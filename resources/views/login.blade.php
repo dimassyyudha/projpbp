3e18084ead48a6edfa9c81a0fcaae8771fc85e3b
Walooni <id.yahyair@gmail.com>
2024-09-18 16:44:34 +0700

    feat: login page, belum selesai

---
+++ resources/views/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-box">
+            <img src="img/download.jpeg" alt="University Loo" class="logo">
+            <h1>SISKARA</h1>
+            <form action="#" method="POST">
+                <input type="text" placeholder="NIM/NIP/Username/Email" required>
+                <input type="password" placeholder="Password" required>
+                <button type="submit">LOGIN</button>
+                <a href="#" class="forgot-password">Forgot password?</a>
+            </form>
+            <p>Don't have an account? <a href="#" class="signup-link">Sign Up</a></p>
+        </div>
+    </div>
+</body>
+</html>
