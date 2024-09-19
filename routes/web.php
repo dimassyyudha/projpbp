cb5005d033e608423186db308a87bcd127015be4
Walooni <id.yahyair@gmail.com>
2024-09-19 14:12:06 +0700

    rapihin repo

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,67 @@
 
 use Illuminate\Support\Facades\Route;
 
+//! Default route
 Route::get('/', function () {
     return view('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+// Semua User
+Route::get('/about', function () {
+    return view('about');
 });
 
 
 
+// Mahasiswa
+Route::get('/dashboard-mhs', function () {
+    return view('dashboard-mhs');
+});
+
+// Pembimbing Akademik
+
+// Bagian Akademik
+Route::get('/dashboard-ba', function () {
+    return view('dashboard-ba');
+});
+
+Route::get('/buatusulan', function () {
+    return view('buatusulan');
+});
+
+Route::get('/daftarusulan', function () {
+    return view('daftarusulan');
+});
+
+Route::get('/aturgelombang', function () {
+    return view('aturgelombang');
+});
 
 
 
+// Dekan
+Route::get('/dashboard-dekan', function () {
+    return view('dashboard-dekan');
+});
+
+Route::get('/usulanruang', function () {
+    return view('usulanruang');
+});
+
+Route::get('/usulanjadwal', function () {
+    return view('usulanjadwal');
+});
+
+// Kaprodi
 
 
 
+//? Testing
 
+Route::get('/test', function () {
+    return view('tailwind');
+});
 
-
+Route::get('/test2', function () {
+    return view('dashboard-gakepake');
+});
